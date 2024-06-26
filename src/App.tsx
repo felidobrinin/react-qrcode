eba1852fdfc436b24cc148ae6f3b7948ed260d43
Felipe Dobrinin <felipedobrinin@gmail.com>
2024-06-26 02:07:18 -0300

    Using RadioGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import qrCodeLogo from "@/assets/qr-code.svg";
+import { useState } from "react";
+
+import QRType from "./utils/utils";
+import RadioGroup from "./components/RadioGroup/RadioGroup";
+
 import "./App.css";
+import qrCodeLogo from "@/assets/qr-code.svg";
 
 function App() {
+  const [qrType, setQRType] = useState(QRType.Text);
+
+  function getQRTypeForm(){
+    switch (qrType) {
+      case QRType.Text:
+        return (<div>Text</div>);
+      case QRType.URL:
+        return (<div>URL</div>);
+      case QRType.Email:
+        return (<div>Email</div>);
+
+      default:
+        return (<div>Text</div>);
+    }
+  }
   return (
     <div className="app">
       <header>
@@ -13,16 +33,9 @@ function App() {
       </header>
 
       <main>
-        <div>
-          <input type="radio" name="type" id="" />
-          <label htmlFor="">Text</label>
-
-          <input type="radio" name="type" id="" />
-          <label htmlFor="">URL</label>
-
-          <input type="radio" name="type" id="" />
-          <label htmlFor="">email</label>
-        </div>
+        <RadioGroup qrType={qrType} setQRType={setQRType}/>
+        {getQRTypeForm()}
+        
       </main>
     </div>
   );
